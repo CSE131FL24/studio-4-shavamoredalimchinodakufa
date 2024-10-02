9c70cc42e667ef23a0b3cfc740cafb1aedebaf4a
joaquinshava <joaquinshava@localhost>
2024-10-02 15:42:27 -0500

    j

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,18 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setPenColor(Color.RED);
+		StdDraw.filledRectangle(0.5, 0.3, .35, .2);
 		
+		StdDraw.setPenColor(Color.YELLOW);
+		StdDraw.filledCircle(0.5, 0.3, .1);
+		
+		 double[] x = {0.5, 0.52, 0.58, 0.53, 0.55, 0.5, 0.45, 0.47, 0.42, 0.48};
+	        double[] y = {0.37, 0.33, 0.33, 0.29, 0.23, 0.27, 0.23, 0.29, 0.33, 0.33};
+
+	        // Draw the star
+	        StdDraw.setPenColor(Color.YELLOW);
+	        StdDraw.filledPolygon(x, y);
+	    
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,23 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+		String shape = in.next();
+        
+        // Read RGB components and fill status
+        int red = in.nextInt();
+        int green = in.nextInt();
+        int blue = in.nextInt();
+        boolean isFilled = in.nextBoolean();
+
+        // Set color
+        StdDraw.setPenColor(red, green, blue);
+
+        // Check if shape is rectangle and draw it
+        if (shape.equals("rectangle")) {
+            double x = in.nextDouble();
+            double y = in.nextDouble();
+            double halfWidth = in.nextDouble();
+            double halfHeight = in.nextDouble();
+
 	}
-}
+	}}
